b1081eba00cd8a1e094b8e3baf3ac29473426b94
Darshan <darshanarkhade@gmail.com>
2023-07-04 01:47:04 +0530

    about done

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Herosection from "@/app/components/Herosection";
+
+const Page = () => {
+    return (
+        <>
+          <Herosection title={"Movie time, together!"} imageUrl={"/home.svg"} />
+        </>
+    );
+};
+
+export default Page;
